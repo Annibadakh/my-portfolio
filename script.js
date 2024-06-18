2fd6260159bbead11b8afa313f4fefc57ef97cfd
Annibadakh <aniketbadakh1212@gmail.com>
2024-06-18 23:53:05 +0530

    Contact form connected

--- script.js
+++ script.js
@@ -48,3 +48,64 @@ function countUp(element, target, duration) {
       }
   }, interval);
 }
+
+
+let formbtn = document.querySelector("#form-btn");
+function loading(flag){
+  (flag) ? (formbtn.innerHTML ="Send Message") : (formbtn.innerHTML = "Submitting.....");
+}
+
+// contact form
+
+loading(true);
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  var form = event.target;
+  var formData = new FormData(form);
+  var loadingBar = document.getElementById('loading-bar');
+
+  // display loading bar
+  loading(false);
+
+
+  fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: {
+          'Accept': 'application/json'
+      }
+  }).then(response => {
+
+    // hide laoding bar
+    loading(true);
+
+
+      if (response.ok) {
+          // Hide the form and show the success message
+          form.reset();
+          var successMessage = document.getElementById('success-message');
+          successMessage.style.display = 'grid';
+          // Hide the success message after 2 seconds
+          setTimeout(function() {
+              successMessage.style.display = 'none';
+          }, 2000);
+      } else {
+          return response.json().then(data => {
+              if (Object.hasOwn(data, 'errors')) {
+                  alert(data["errors"].map(error => error["message"]).join(", "));
+              } else {
+                  alert("Oops! There was a problem submitting your form");
+              }
+          });
+      }
+  }).catch(error => {
+      alert("Oops! There was a problem submitting your form");
+  });
+});
+
+
+
+
+
+  
